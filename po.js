3027993bd955621d6a3fdd208965d7b7ce201f75
omarmojahed <53019445+omarmojahed@users.noreply.github.com>
2019-07-31 08:31:21 +0200

    first upload

---
+++ po.js
@@ -0,0 +1,181 @@
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// 222222222222222222222222222222222 contact us 222222222222222222222222222222222222222
+
+
+
+        $(".input").focus(function() {
+      $(this).parent().addClass("focus");
+    })
+    
+
+
+///////////////////////////////////////////sec nav ///////////////////////////////////////
+
+/////////////////////////////////////////////// sec nav ///////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
